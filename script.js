9e730f51a4edb9d0a3e3acb5c270f0669cd41018
rafiwira <86337682+rafiwira@users.noreply.github.com>
2021-06-23 12:56:45 +0800

    Edited HTML, JS and CSS HTML: - Bootstrap template JS: - Read README.md. - Markdown parser. CSS: - body background color.

---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementsByTagName("body").onLoad = readTextFile();
+
+// code by https://stackoverflow.com/a/14446538
+function readTextFile() {
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", "/README.md", false);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4) {
+      if (rawFile.status === 200 || rawFile.status == 0) {
+        var allText = rawFile.responseText;
+        console.log("rTF pass");
+        let display = document.getElementById("story");
+        display.innerHTML = parseMarkdown(allText);
+      }
+    }
+  };
+  rawFile.send(null);
+}
+
+// code by https://www.bigomega.dev/markdown-parser
+function parseMarkdown(markdownText) {
+  const htmlText = markdownText
+    .replace(/^### (.*$)/gim, "<h3>$1</h3>")
+    .replace(/^## (.*$)/gim, "<h2>$1</h2>")
+    .replace(/^# (.*$)/gim, "<h1>$1</h1>")
+    .replace(/^\> (.*$)/gim, "<blockquote>$1</blockquote>")
+    .replace(/\*\*(.*)\*\*/gim, "<b>$1</b>")
+    .replace(/\*(.*)\*/gim, "<i>$1</i>")
+    .replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />")
+    .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
+    .replace(/\n$/gim, "<br />");
+
+  return htmlText.trim();
+}
